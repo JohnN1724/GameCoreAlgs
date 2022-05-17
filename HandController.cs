5f9bf80c11cf198c49d26d4bc8ce4a97b54c62f8
John Nuttall <61627001+JohnN1724@users.noreply.github.com>
2022-05-17 14:09:32 +0100

    Create HandController.cs
    Hand Controller takes in trigger values for animation of hands

---
+++ HandController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField] InputActionReference controllerActionGrip;
+    [SerializeField] InputActionReference controllerActionTrigger;
+
+    private Animator handAnimator;
+
+    private void Awake() 
+    {
+        controllerActionGrip.action.performed += GripPress;
+        controllerActionTrigger.action.performed += TriggerPress;
+
+        handAnimator = GetComponent<Animator>();
+    }
+
+    private void GripPress(InputAction.CallbackContext obj) => handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+    private void TriggerPress(InputAction.CallbackContext obj) => handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+    
+}
