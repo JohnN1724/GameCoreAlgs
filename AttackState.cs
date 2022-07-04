757632cb73ec5655887f3e4357546c8ed3e52e49
John Nuttall <61627001+JohnN1724@users.noreply.github.com>
2022-07-04 22:07:15 +0100

    Added Basic AI State Scripts

---
+++ AttackState.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttackState : StateMachineBehaviour
+{
+    
+    NavMeshAgent agent;
+    Transform player;
+    Transform weapon;
+    
+    float attackRange = 2.5f;
+    float weaponRange = 2.0f;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Gets Nav mesh for AI as well as identifying the player
+        
+        agent = animator.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        weapon = GameObject.FindGameObjectWithTag("Weapon").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        // Gets distance from the player which the AI will chase until the player is out of the chase range
+        
+        float distance = Vector3.Distance(animator.transform.position, player.position);
+
+        float weaponDist = Vector3.Distance(animator.transform.position, weapon.position);
+
+        // AI will begin to attack player if they're close enough to the player
+        
+        if(distance < attackRange)
+        {
+            animator.SetBool("attackPlayer", true);
+        }
+      /*  else
+        {
+            animator.SetBool("moveToPlayer", true);
+
+            // Sets destination for the player to "chase"
+        
+            agent.SetDestination(player.position);
+
+        } */
+
+        // If AI is hit with the player's weapon
+
+        if(weaponDist < weaponRange)
+        {
+            animator.SetBool("aiDead", true);
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+}
+
---
+++ MoveState.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveState : StateMachineBehaviour
+{
+    
+    NavMeshAgent agent;
+    Transform player;
+    Transform weapon;
+    
+    float attackRange = 2.5f;
+    float weaponRange = 2.0f;
+    float engageRange = 10.0f;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Gets Nav mesh for AI as well as identifying the player
+        
+        agent = animator.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        weapon = GameObject.FindGameObjectWithTag("Weapon").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        // Gets distance from the player which the AI will chase until the player is out of the chase range
+        
+        float distance = Vector3.Distance(animator.transform.position, player.position);
+
+        float weaponDist = Vector3.Distance(animator.transform.position, weapon.position);
+
+        agent.SetDestination(player.position);
+
+        // Player moves out of range of AI
+
+        if(distance > engageRange)
+        {
+            animator.SetBool("moveToPlayer", false);
+        }
+        
+        
+        // AI will begin to attack player if they're close enough to the player
+        
+        if(distance < attackRange)
+        {
+            animator.SetBool("attackPlayer", true);
+        }
+
+
+        if(weaponDist < weaponRange)
+        {
+            animator.SetBool("aiDead", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+}
